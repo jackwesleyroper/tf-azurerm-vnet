0cc8b532abd5ca6ea518719d493056426bbd5add
Jack Roper <39914569+jackwesleyroper@users.noreply.github.com>
2025-02-04 11:07:40 +0000

    fmt

--- main.tf
+++ main.tf
@@ -12,12 +12,12 @@ resource "azurerm_virtual_network" "vnet" {
   name          = var.name
   address_space = var.address_space
   dns_servers   = var.dns_servers
-  
-  tags  = var.tags
+
+  tags = var.tags
 
 
   dynamic "ddos_protection_plan" {
-    for_each = var.is_ddos_enabled == true ? [1] :[]
+    for_each = var.is_ddos_enabled == true ? [1] : []
 
     content {
       id     = var.ddos_protection_plan_id
